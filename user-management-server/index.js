49d90d0ad115d74cff0c76f9aa5d1c1ed1408728
unknown <mdrafiqulakhter0@gmail.com>
2026-05-01 16:04:37 +0600

    Fix: stop tracking ignored files

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -1,36 +1,69 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const cors = require('cors')
 const port = process.env.PORT || 3000;
-
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/', (req, res) => {
-    res.send('server is running')
-})
-
+// lBe8XdFRWKIIzv9v
 const users = [
     { id: 1, name: 'sabana', email: 'sabana@gmail.com' },
     { id: 2, name: 'sabnoor', email: 'sabnoor@gmail.com' },
     { id: 3, name: 'sabiha', email: 'sabiha@gmail.com' }
 ]
 
-app.get('/users', (req, res) => {
-    res.send(users)
+app.use(cors())
+app.use(express.json())
+
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        // await client.db('admin').command({ping:1})
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        app.get('/users', (req, res) => {
+            res.send(users)
+        })
+
+        app.post('/users', (req, res) => {
+            console.log('users post method')
+            console.log(req.body)
+            const newUser = req.body;
+            newUser.id = users.length + 1;
+            //add data to the database
+            users.push(newUser)
+            res.send(newUser)
+
+        })
+
+
+        app.delete('/delete/:id', (req, res) => {
+            const id = req.params.id;
+            console.log('to be deleted', id)
+        })
+
+    } finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('server is running')
 })
 
-app.post('/users',(req,res)=>{
-    console.log('users post method')
-    console.log(req.body)
-    const newUser = req.body;
-    newUser.id = users.length + 1;
-    //add data to the database
-    users.push(newUser)
-    res.send(newUser)
 
-})
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
